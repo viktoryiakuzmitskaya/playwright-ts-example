63b1dd15d523c43d75ab4c15dfbdc23ad8620a8d
Viktoryia Kuzmitskaya <kuzmitskaya.viktoryia@gmail.com>
2025-03-26 07:54:10 +0700

    Updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    headless: false,
+    //headless: false,
     screenshot: "only-on-failure",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
